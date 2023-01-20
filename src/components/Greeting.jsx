34a5d9cd344b0531b22b81ab643ee165a187d834
Anas <anas.chaabane98@gmail.com>
2023-01-20 16:41:40 +0100

    readme

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,13 +1,7 @@
 import { useGetGreetingsQuery } from "./api/apiSlice";
 
 const Greeting = () => {
-  const {
-    data: greeting,
-    isLoading,
-    isSuccess,
-    isError,
-    error,
-  } = useGetGreetingsQuery();
+  const { data: greeting } = useGetGreetingsQuery();
 
   return greeting ? <div>{greeting.message}</div> : <p> no greetings yet ! </p>;
 };

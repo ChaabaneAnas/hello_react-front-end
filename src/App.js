70961c22efe1144accef24cf6b4b8e749cac59f1
Anas <anas.chaabane98@gmail.com>
2023-01-20 16:48:55 +0100

    add link

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Greeting from "./components/Greeting";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/greeting" element={<Greeting />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/greetings" element={<Greeting />} />
         </Routes>
       </Router>
     </div>
